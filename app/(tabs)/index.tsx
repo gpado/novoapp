9a74455096ef2b71a2423d8eaa273d991074aa59
Gabriel Padoveze <53236759+gpado@users.noreply.github.com>
2024-08-31 10:16:07 -0300

    melhoria

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';  // Importar Text
 import { ThemedText } from '@/components/ThemedText';
 import { WebView } from 'react-native-webview';
 
@@ -7,26 +7,31 @@ export default function HomeScreen() {
   const youtubeURL = `https://www.youtube.com/embed/${youtubeVideoId}?autoplay=1&controls=0&loop=1&playlist=${youtubeVideoId}`;
 
   return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <ThemedText type="title" style={styles.centeredTitle}>MobCidade</ThemedText>
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <ThemedText type="title" style={styles.centeredTitle}>MobCidade</ThemedText>
+        </View>
+        <View style={styles.videoContainer}>
+          <WebView
+            source={{ uri: youtubeURL }}
+            style={{ flex: 1 }}
+            allowsFullscreenVideo={true}
+            javaScriptEnabled={true}
+          />
+        </View>
       </View>
-      <View style={styles.videoContainer}>
-        <WebView
-          source={{ uri: youtubeURL }}
-          style={{ flex: 1 }}
-          allowsFullscreenVideo={true}
-          javaScriptEnabled={true}
-        />
+      <View style={styles.transportTitleContainer}>
+        <Text style={styles.transportTitle}>Principais Transportes</Text>
       </View>
     </View>
-
-
-
   );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,  // Garante que o container principal preencha a tela
+  },
   titleContainer: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -41,8 +46,16 @@ const styles = StyleSheet.create({
     height: 200,
     width: '100%',
   },
-  container:{
-    backgroundColor: '#5DB075', // Cor de fundo verde
-    padding: 20, // Espaçamento interno
-  }
+  container: {
+    backgroundColor: '#5DB075',  // Cor de fundo verde
+    padding: 20,  // Espaçamento interno
+  },
+  transportTitleContainer: {
+    padding: 20,
+  },
+  transportTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+  },
 });
