230f5582ca405148a51f0b0b9fe150ae0ab2090c
Gabriel Padoveze <53236759+gpado@users.noreply.github.com>
2024-08-31 12:02:03 -0300

    ok

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, ActivityIndicator, Text } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { ThemedText } from '@/components/ThemedText';
-import { LocationObject } from 'expo-location';
+import { LocationObject } from 'expo-location'; // Ensure that LocationObject is correctly imported
 
 export default function TabTwoScreen() {
+  // Explicitly define the type for location and errorMsg
   const [location, setLocation] = useState<LocationObject | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          setLoading(false);
+          return;
+        }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+      } catch (error) {
+        setErrorMsg('Failed to retrieve location');
+      } finally {
+        setLoading(false);
+      }
     })();
   }, []);
 
@@ -27,18 +36,23 @@ export default function TabTwoScreen() {
       <View style={styles.titleContainer}>
         <ThemedText type="title" style={styles.centeredTitle}>MobCidade</ThemedText>
       </View>
-      {location ? (
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : location ? (
         <MapView
           provider={PROVIDER_GOOGLE}
           style={styles.mapStyle}
           initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
+            latitude: -23.5505,  // Latitude de São Paulo
+            longitude: -46.6333, // Longitude de São Paulo
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
+        
           }}
         />
-      ) : null}
+      ) : (
+        <Text>{errorMsg || 'Unable to display the map'}</Text> // Using Text instead of ThemedText for simplicity
+      )}
     </View>
   );
 }
@@ -60,6 +74,6 @@ const styles = StyleSheet.create({
   },
   mapStyle: {
     width: Dimensions.get('window').width - 40,
-    height: 300,
+    flex: 1,
   },
 });
